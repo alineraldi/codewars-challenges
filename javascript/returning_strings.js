ee819657ce0d348d198d82ef81b2dca97b718a97
Aline <alineralif@gmail.com>
2024-08-30 12:24:39 -0300

    returning_strings first commit

---
+++ javascript/returning_strings.js
@@ -0,0 +1,9 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name){
+    return (`Hello, ${name} how are you doing today?`)
+  }
+  
+  let name = "Ryan"
+  let result = greet(name)
+  console.log(result)
