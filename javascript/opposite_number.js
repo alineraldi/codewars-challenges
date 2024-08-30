a67e10d4a38ed1db790ceb7dc96a8ae919e4cb17
Aline <alineralif@gmail.com>
2024-08-30 12:28:36 -0300

    opposite number first commit

---
+++ javascript/opposite_number.js
@@ -0,0 +1,9 @@
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+function opposite(number) {
+    return (number * -1)
+  }
+ 
+let number = 1
+let result = opposite(number)
+console.log(result)
