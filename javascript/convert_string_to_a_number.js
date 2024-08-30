de5f4cd8d0ad08364eed58b0f2ba6b51d629482b
Aline <alineralif@gmail.com>
2024-08-30 12:17:05 -0300

    convert_string first commit

---
+++ javascript/convert_string_to_a_number.js
@@ -0,0 +1,9 @@
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+function stringToNumber(str){
+    return Number(str);
+  }
+  
+  let str = "1234"
+  let result = stringToNumber(str)
+  console.log(result)
